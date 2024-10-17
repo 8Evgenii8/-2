8e8ceb46d25d19399c05a809ebeabac58e0fc5df
8Evgenii8 <jiganpro77@gmail.com>
2024-10-17 17:50:57 +0300

    Add files via upload

---
+++ DZ1.py
@@ -0,0 +1,6 @@
+tasks = 12
+hours = 1.5
+course_name = 'Python'
+print(hours / tasks)
+nev_hours = 0.125
+print('Курс:',course_name,',всего задач:',tasks,',затрачено часов:',hours,',среднее время выполнения',nev_hours,'.')
